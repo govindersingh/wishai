478aaee491fe629e199d963017c75262e8b67240
govindersingh <govindersingh05@gmail.com>
2022-06-30 12:27:08 +0530

    connect with heroku

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,8 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Share;
+use App\Models\Cart;
+use App\Models\Customers;
+use App\Models\Products;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
-    //
+    public function index(){
+        
+    }
+
+    public function activity(){
+
+    }
+    
+    public function customers(){
+
+    }
+
+    public function products(){
+
+    }
+
+    public function settings(){
+
+    }
+
+    public function support(){
+
+    }
+
+    public function help(){
+
+    }
 }
